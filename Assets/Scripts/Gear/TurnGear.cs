261a765c86b61635c20fa4d99d7858ec3397e954
rdvhoorn <38728066+rdvhoorn@users.noreply.github.com>
2022-12-18 19:15:01 +0100

    Added gears!

---
+++ Assets/Scripts/Gear/TurnGear.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Netcode;
+using UnityEngine;
+
+public class TurnGear : NetworkBehaviour
+{
+
+    public float rotationalSpeed = 20f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateGearServerRpc();
+    }
+
+    [ServerRpc(RequireOwnership = false)]
+    void UpdateGearServerRpc() {
+        transform.Rotate(Vector3.right * rotationalSpeed * Time.deltaTime);
+    }
+}
